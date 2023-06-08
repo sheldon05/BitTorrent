a53b8bf14403bd2a9faf8a016ad90fc008406525
sheldon05 <jesussantoscapote@gmail.com>
2023-06-08 00:30:13 -0400

    add connect to tracker method

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import Pyro4
 import os
-from torrent_files_utils import torrent_creator, torrent_reader
+from torrent_files_utils.torrent_creator import TorrentCreator
+from torrent_files_utils.torrent_reader import TorrentReader
 
 class BitTorrentClient:
     
@@ -9,20 +10,37 @@ class BitTorrentClient:
         self.port = port
         os.mkdir(os.path.join(os.getcwd(), 'client_files', f'{ip}:{port}downloads'))
 
-    
-        #TODO:Put more trackers on .torrent    
+        #TODO:Put more trackers on .torrent 
+
+           
     def upload_file(self, path, tracker_url, private = false, comments = "unknow", source = "unknow" ):
         '''
         Upload a local file to the tracker
         '''
-        tc = torrent_creator(path, 1 << 18, private, [tracker_url], comments, source )
+        tc = TorrentCreator(path, 1 << 18, private, [tracker_url], comments, source )
         sha1_hash = tc.get_hash_pieces()
+
         #TODO:Let tracker now that this file is upload, this part is with pyro Chuchi
         tc.create_dottorrent_file('torrent_files')
+
 
     def dowload_file(self,dottorrent_file_path):
         '''
         Start dowload of a file from a local dottorrent file
         '''
-        tr = torrent_reader(dottorrent_file_path)
+        tr = TorrentReader(dottorrent_file_path)
         info = tr.build_torrent_info()
+
+
+    def connect_to_tracker(self, tracker_ip, tracker_port):
+        #by default all the trackers have the service name tracker
+        uri = f"PYRO:tracker@{tracker_ip}:{tracker_port}"
+        tracker_proxy = Pyro4.Proxy(uri)
+
+        try:
+            tracker_proxy._pyroConnection.ping()
+            print("El servidor Pyro está activo.")
+        except Pyro4.errors.CommunicationError:
+            print("Error: El servidor Pyro no está activo.")
+
+        return tracker_proxy
--- tracker.py
+++ tracker.py
@@ -1,13 +1,9 @@
 import Pyro4
 
-
-
 class Tracker:
 
     def __init__(self, ip, port):
-
         self.ip = ip
-
         self.port = port
 
         # keys are the concatenation of sha1 hash of the pieces of the files, pieces key in .torrent
@@ -16,41 +12,31 @@ class Tracker:
 
 
     def get_peers(self, pieces_sha1):
-
         peers = self.database[pieces_sha1]
 
         return peers
 
     
     def add_to_database(self, pieces_sha1, ip, port):
-
         if pieces_sha1 in self.database.keys():
-
             if not self.database[pieces_sha1].contains((ip, port)):
-
                 self.database[pieces_sha1].append((ip, port))
 
         else:
-
             self.database[pieces_sha1] = [(ip, port)]
 
 
-
     def remove_from_database(self, pieces_sha1, ip, port):
-
         if pieces_sha1 in self.database.keys():
-
             if self.database[pieces_sha1].contains((ip, port)):
-
                 self.database[pieces_sha1].remove((ip, port))
 
 
-daemon = Pyro4.Daemon(host="127.0.0.1")
+tracker = Tracker("127.0.0.1", 6200)
 
+daemon = Pyro4.Daemon(host=tracker.ip, port= tracker.port)
 ns = Pyro4.locateNS()
-tracker = Tracker(ip, port)
-uri = daemon.register(servidor)
-ns.register("nombre_del_servicio", uri) 
-daemon.requestLoop() 
-
-
+uri = daemon.register(tracker)
+ns.register("tracker", uri)
+print(f"TRACKER {tracker.ip}:{tracker.port} STARTED")
+daemon.requestLoop()
