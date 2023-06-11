22eaa5b2851ed79e6d4ad149029a692fcc95b6fc
sheldon05 <jesussantoscapote@gmail.com>
2023-06-11 10:59:30 -0400

    testing things

--- testing.py
+++ testing.py
@@ -1,3 +1,6 @@
+import base64
+from hashlib import sha1
+
 class Prueba:
     def __init__(self, a, b):
         self.a = a
@@ -13,4 +16,15 @@ class Prueba:
     def __str__(self):
         return 'soy la clase prueba'
 
-    
+a = 'XrY7u+Ae7tCTyyK7j1rNww=='
+b = b'^\xb6;\xbb\xe0\x1e\xee\xd0\x93\xcb"\xbb\x8fZ\xcd\xc3'
+
+c = base64.b64decode(a)
+
+string = 1
+
+string1 = sha1(string).digest()
+print(len(string1))
+
+string1 = sha1(string).hexdigest()
+print(len(string1))
