2802d3de334d8960fa68ac811154edcc1a5e887b
sheldon05 <jesussantoscapote@gmail.com>
2023-06-11 21:42:06 -0400

    finished distribute information

--- tracker.py
+++ tracker.py
@@ -17,120 +17,6 @@ class Tracker(object):
         # values ip and port of the peers that potentially have the piece  , list of tuples (ip,port)
         self.database = {}
 
-<<<<<<< HEAD
-    def run_chord(self):
-        self.stabilize()
-        self.fix_finger()
-        self.check_predecessor()
-        print('esta es la finger_table')
-        print(self.finger_table)
-        Timer(1, self.run_chord, []).start()
-        
-    
-    def join(self, ip, port) -> None:
-        tracker_proxy = self.connect_to(ip, port, 'tracker')
-        print(str(self.node_id) + 'algo!!!!!!!!!!!!!!')
-        self.successor = tracker_proxy.find_successor(self.node_id)[1]
-
-
-    def distribute_information():
-        
-        
-
-
-    # def find_predecessor(self, key):
-    #     node_id = self.node_id
-    #     sucessor = sha256_hash(self.successor)
-    #     while key not in range(node_id+1, sucessor+1):
-    #         node_id = self.closest_preceding_finger(key)
-    #         tracker_proxy = self.connect_to(node_id.split(":")[0], node_id.split(":")[1], 'tracker')
-    #         sucessor = self.sha256_hash(tracker_proxy.get_succesor())
-    #     return node_id
-
-    def closest_preceding_finger(self, id):
-        for i in range(len(self.finger_table)-1, -1, -1):
-            if self.finger_table[i][0] in range(self.node_id+1, id):
-                return self.finger_table[i]
-        return [self.node_id,self.ip+':'+str(self.port)]
-
-    @Pyro4.expose
-    def find_successor(self, key):
-        print("find succesor")
-        print(self.successor)
-        successor = self.successor
-        node_id = self.node_id
-        if node_id == sha256_hash(successor):
-            return [node_id, self.ip+":"+str(self.port)]
-        print(node_id)
-        print(key)
-        print(sha256_hash(successor))
-        if key in range(node_id+1, sha256_hash(successor)+1):
-            print("sucessor:" + successor)
-            return (sha256_hash(successor), successor)
-        elif sha256_hash(self.predecessor) > self.node_id:
-            return [node_id, self.ip+":"+str(self.port)] 
-        else:
-            node_id = self.closest_preceding_finger(key)[1]
-            tracker_proxy = self.connect_to(node_id.split(":")[0], int(node_id.split(":")[1]), 'tracker')
-            return tracker_proxy.find_successor(key)
-    
-    def stabilize(self):
-        print("stabilize")
-        print(self.successor)
-        print(self.predecessor)
-        tracker_proxy = self.connect_to(self.successor.split(":")[0], int(self.successor.split(":")[1]), 'tracker')
-        successor_predecessor = tracker_proxy.get_predecessor()
-
-        if sha256_hash(successor_predecessor) in range(self.node_id+1, sha256_hash(self.successor)) or (self.predecessor != "" and sha256_hash(self.successor)==self.node_id):
-            self.successor = successor_predecessor
-        
-        tracker_proxy = self.connect_to(self.successor.split(":")[0], int(self.successor.split(":")[1]), 'tracker')
-        print('voy a entrar a notificar')
-        tracker_proxy.notify(self.ip+":"+str(self.port))
-
-    @Pyro4.expose
-    def notify(self, node):
-        print("notify")
-        print('Estoy printeando node: %s', node)
-
-        if not self.predecessor or sha256_hash(self.predecessor) == self.node_id or sha256_hash(node) in range (sha256_hash(self.predecessor), self.node_id):
-            self.predecessor = node
-        print(self.successor)
-        print(self.predecessor)
-
-
-    def fix_finger(self):
-        print("fix_finger")
-        self.next_to_fix += 1
-        if self.next_to_fix > 3:
-            self.next_to_fix = 1
-
-        self.finger_table[self.next_to_fix] = self.find_successor(self.node_id+2**(self.next_to_fix-1))
-        
-
-    def check_predecessor(self):
-        print("check predecessor")
-        try:
-            tracker_proxy = self.connect_to(self.predecessor.split(":")[0], int(self.predecessor.split(":")[1]), 'tracker')
-            print("predeccesor checked")
-        except:
-            self.predecessor = ""
-    
-    @Pyro4.expose
-    def get_successor(self):
-        return self.successor
-
-    @Pyro4.expose
-    def get_predecessor(self):
-        return self.predecessor
-   
-    
-
-   
-
-
-=======
->>>>>>> 5a27bd1cda24ac7742f7a60e5c362ce92b7ad5d9
     @Pyro4.expose
     def get_peers(self, pieces_sha1):
         peers = self.database[pieces_sha1]
@@ -154,10 +40,19 @@ class Tracker(object):
             if not (ip,port) in self.database[pieces_sha1]:
                 self.database[pieces_sha1].remove((ip, port))
 
+
     def add_to_trackers(self, pieces_sha1, ip, port):
         pieces_sha256 = sha256_hash(pieces_sha1)
         if self.sucessor == '':
             self.add_to_database(pieces_sha256, ip, port)
+
+    
+    def distribute_information(self):
+        for pieces_sha256, ip, port in self.database.items():
+            owner_ip, owner_port = self.find_successor(pieces_sha256).split(':')
+            owner_proxy = self.connect_to(owner_ip, int(owner_port), 'tracker')
+            owner_proxy.add_to_database(pieces_sha256, ip, port)
+
 
     def join(self, ip, port):
         proxy_tracker = self.connect_to(ip, port, 'tracker')
