7f483afb3d4de355993e8a5a72b98ec95354a97b
sheldon05 <jesussantoscapote@gmail.com>
2023-06-14 01:32:35 -0400

    docs added

--- node2.py
+++
@@ -1,7 +0,0 @@
-import Pyro4
-
-# Buscamos la URI del objeto MyObject registrado en el otro nodo
-uri = Pyro4.Proxy("PYRO:myobject.node1@172.20.0.2:9091")
-
-# Llamamos al método hello() del objeto remoto
-print(uri.hello())
